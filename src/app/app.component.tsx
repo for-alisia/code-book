940e98c891af20780c55430949647945e49f5a5d
for-alisia <for-alisia@yandex.ru>
2021-02-22 08:35:25 +0300

    Started work on plugin for ESBuild

---
+++ src/app/app.component.tsx
@@ -0,0 +1,48 @@
+import * as esbuild from 'esbuild-wasm';
+import { useState, useRef, useEffect } from 'react';
+
+/** Plugins */
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+
+const App: React.FC = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+export default App;
