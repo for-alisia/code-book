73bcf67daed2711156c8dcf86adc01fa7a94556b
for-alisia <for-alisia@yandex.ru>
2021-02-23 19:37:02 +0300

    Added execution of the code in an iframe

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -7,8 +7,8 @@ import { fetchPlugin } from '../plugins/fetch-plugin';
 
 const App: React.FC = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -26,6 +26,8 @@ const App: React.FC = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -36,16 +38,43 @@ const App: React.FC = () => {
         global: 'window',
       },
     });
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector("#root");
+              root.innerHTML='<div style="color: red;"><h4>Runtime error:</h4>' + err + '</div>';
+              console.error(err) ;
+            }
+          }, false)
+        </script>
+      </body>
+    </html>
+`;
 
   return (
     <div>
-      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <textarea
+        value={input}
+        rows={10}
+        style={{ width: '300px' }}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+
+      <iframe ref={iframe} srcDoc={html} title="code-frame" sandbox="allow-scripts" />
     </div>
   );
 };
