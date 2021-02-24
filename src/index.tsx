b6102932805e13ec930786bde362ef4504d3a132
for-alisia <for-alisia@yandex.ru>
2021-02-24 19:50:57 +0500

    Changed README.md

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom';
 /** Components */
 import App from './app/app.component';
 
+/** Styles */
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
